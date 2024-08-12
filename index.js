8817aac4abdbf5edd3028c56be711eee77e51eea
Luis Alejandro <128195940+TWRebot@users.noreply.github.com>
2024-08-12 22:54:23 +0000

    12/08/24

---
+++ index.js
@@ -0,0 +1,9 @@
+const idade = 17;
+const anoNascimento = 2007;
+let anoAtual = 2024;
+
+anoAtual = anoAtual + 1;
+
+  console.log (idade);
+  console.log (anoAtual);
+                                              
